4b1b8cb110b5617c7a5e393dba48648973610917
JothishJJ <jothishjj11@gmail.com>
2024-04-22 23:45:13 +0530

    Add App route

---
+++ app/app/page.tsx
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <div>
+      <h2>Reccomended For You!</h2>
+      <p>Please like some books to figure out your books</p>
+    </div>
+  )
+}
+
+export default App
+
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,15 @@
+import Link from "next/link"
+
 export default function Home() {
   return (
     <main>
-      <h1>Hello World!</h1>
+      <section className="flex flex-col items-center justify-center h-screen">
+        <h1>Find the right book to Read!</h1>
+        <br />
+        <Link href="/app">
+          <button className="p-2 border-black border-2 bg-black text-white rounded-lg">Explore the Books!</button>
+        </Link>
+      </section>
     </main>
   );
 }
