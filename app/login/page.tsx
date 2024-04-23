9d875d2872f3beb5364278156c44d006cf480dbe
JothishJJ <jothishjj11@gmail.com>
2024-04-23 20:18:09 +0530

    Change the Login Page

---
+++ app/login/page.tsx
@@ -0,0 +1,26 @@
+import { auth } from "@/auth"
+import Button from "@/components/Button"
+import { redirect } from "next/navigation"
+
+const SignIn = async () => {
+
+  const session = await auth()
+
+  if (!session?.user)
+    return (
+      <div className="grid lg:grid-cols-2 grid-cols-1 lg:px-80 items-center justify-center min-h-screen">
+        <form className="flex flex-col gap-8">
+          <Button className="w-96">Sign In With Google</Button>
+          <Button className="w-96">Sign In With GitHub</Button>
+        </form>
+
+        <div className="text-center">
+          <p>Login To get choose your next Book!</p>
+        </div>
+      </div>
+    )
+  else redirect("/app")
+}
+
+export default SignIn
+
