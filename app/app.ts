0d2e74372c73b6c94b6b7f99b8e0044b63804465
JothishJJ <jothishjj11@gmail.com>
2024-04-23 13:00:17 +0530

    Add Auth.js and Signin With Github

---
+++ app/app.ts
@@ -0,0 +1,6 @@
+import NextAuth from "next-auth"
+
+export const { handlers, signIn, signOut, auth } = NextAuth({
+  providers: [],
+})
+
--- app/app/page.tsx
+++ app/app/page.tsx
@@ -1,9 +1,39 @@
-const App = () => {
-  return (
+import { auth } from "../app"
+import { signIn, signOut } from "@/auth"
+
+const App = async () => {
+
+  const session = await auth()
+
+  if (!session?.user) return (
     <div>
       <h2>Reccomended For You!</h2>
       <p>Please like some books to figure out your books</p>
+      <br />
+      <form
+        action={async () => {
+          "use server"
+          await signIn("github")
+        }}
+      >
+        <button type="submit">Signin with GitHub</button>
+      </form>
     </div>
+  )
+
+  return (
+    <div>
+      <h2>You're signed in!</h2>
+      <p>Hello, there {session.user.name}</p>
+      <br />
+      <form
+        action={async () => {
+          "use server"
+          await signOut()
+        }}
+      >
+        <button type="submit">Sign Out</button>
+      </form>    </div>
   )
 }
 
