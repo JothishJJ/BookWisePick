bb90f36e70874104586266fb4928bd8ca16f56dc
JothishJJ <jothishjj11@gmail.com>
2024-04-23 21:09:55 +0530

    Remove redirect in page

--- app/app/page.tsx
+++ app/app/page.tsx
@@ -1,18 +1,10 @@
-import { redirect } from "next/navigation"
-import { auth } from "../app"
-
 const App = async () => {
-  const session = await auth()
-
-  if (session?.user) return (
+  return (
     <div className="px-80">
       <h2>Reccomended For You!</h2>
       <p>Please like some books to figure out your books</p>
     </div>
   )
-  else {
-    redirect("/login")
-  }
 }
 
 export default App
--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,25 +1,18 @@
-import { auth } from "@/auth"
 import Button from "@/components/Button"
-import { redirect } from "next/navigation"
 
 const SignIn = async () => {
+  return (
+    <div className="grid lg:grid-cols-2 grid-cols-1 lg:px-80 items-center justify-center min-h-screen">
+      <form className="flex flex-col gap-8">
+        <Button className="w-96">Sign In With Google</Button>
+        <Button className="w-96">Sign In With GitHub</Button>
+      </form>
 
-  const session = await auth()
-
-  if (!session?.user)
-    return (
-      <div className="grid lg:grid-cols-2 grid-cols-1 lg:px-80 items-center justify-center min-h-screen">
-        <form className="flex flex-col gap-8">
-          <Button className="w-96">Sign In With Google</Button>
-          <Button className="w-96">Sign In With GitHub</Button>
-        </form>
-
-        <div className="text-center">
-          <p>Login To get choose your next Book!</p>
-        </div>
+      <div className="text-center">
+        <p>Login To get choose your next Book!</p>
       </div>
-    )
-  else redirect("/app")
+    </div>
+  )
 }
 
 export default SignIn
